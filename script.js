98d32e7a06484b46e0f7362bbe9dffed6b5e4af9
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-09-30 14:50:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const birthdayCard = document.getElementById('birthdayCard');
-    const backContent = birthdayCard.querySelector('.back');
-
-    birthdayCard.addEventListener('click', function() {
-        birthdayCard.classList.toggle('flipped');
-        // Toggle visibility of back content to ensure it's displayed only when flipped
-        if (birthdayCard.classList.contains('flipped')) {
-            backContent.classList.remove('hidden');
-        } else {
-            backContent.classList.add('hidden');
-        }
-    });
+document.getElementById('open-card-btn').addEventListener('click', function() {
+    const cardContainer = document.querySelector('.card-container');
+    cardContainer.classList.toggle('card-flip');
 });
+
+
+
+
+
+
+ 
