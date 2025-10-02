e5cbabecba3e769c32709d5d33df66d707006015
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-10-02 08:23:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function showSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'flex'
+        }
+        function hideSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+        }
+
 document.addEventListener('DOMContentLoaded', () => {
     const track = document.querySelector('.carousel-track');
     const slides = Array.from(track.querySelectorAll('.carousel-slide'));
