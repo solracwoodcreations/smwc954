da409ca8513841824fc8f023e2bba8d9f87eb757
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-10-03 15:18:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //hamburger menu
-function showSidebar(){
-            const sidebar = document.querySelector('.sidebar')
-            sidebar.style.display = 'flex'
-        }
-        function hideSidebar(){
-            const sidebar = document.querySelector('.sidebar')
-            sidebar.style.display = 'none'
-        }
+//function showSidebar(){
+           // const sidebar = document.querySelector('.sidebar')
+            //sidebar.style.display = 'flex'
+       // }
+        //function hideSidebar(){
+            //const sidebar = document.querySelector('.sidebar')
+            //sidebar.style.display = 'none'
+        //}
 
 
 //birthday card
